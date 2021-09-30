083a7faa267309eecda3a7a49364c2953bf82c31
RafliRasyidin <raflirasyidin20@gmail.com>
2021-09-30 23:10:33 +0700

    add constant value

---
+++ app/src/main/java/com/babakbelur/e_maktabah/core/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.babakbelur.e_maktabah.core.utils
+
+object Constants {
+
+    const val BASE_URL = "http://apimuslimify.herokuapp.com/"
+
+    const val REQUEST_TIMEOUT = 15L
+
+    const val DATABASE_NAME = "e_maktabah.db"
+}
